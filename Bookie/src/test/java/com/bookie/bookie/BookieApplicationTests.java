e37b3a930c85edea1efbbed18f7909c59cc38e2a
soufiane <soufianeajaite2@gmail.com>
2025-10-21 15:34:12 +0100

    Init

---
+++ Bookie/src/test/java/com/bookie/bookie/BookieApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bookie.bookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookieApplicationTests {
+
+    @Test
+    void contextLoads() {
+//        to add later
+    }
+
+}
