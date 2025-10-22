cea7819fd70aab2d66569829716aac765f2a8e23
soufiane <soufianeajaite2@gmail.com>
2025-10-22 18:14:24 +0100

    configure database and flyway

---
+++ Bookie/src/main/java/com/bookie/bookie/advices/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.bookie.bookie.advices;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleResourceNotFound(NoSuchElementException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
